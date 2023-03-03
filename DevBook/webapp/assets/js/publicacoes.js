f2c8eaacbfd346bbc5c9671394eec0e115d124a4
Daniel Santos <dmsdaniel@gmail.com>
2023-03-03 20:00:14 -0300

    curtir publicaco

--- DevBook/webapp/assets/js/publicacoes.js
+++ DevBook/webapp/assets/js/publicacoes.js
@@ -1,5 +1,6 @@
 $('#nova-publicacao').on('submit',criarPublicacao)
-$('.curtir-publicacao').on('click',curtirPublicacao)
+$(document).on('click', '.curtir-publicacao', curtirPublicacao);
+$(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
 function criarPublicacao(evento){
     evento.preventDefault()
     $.ajax({
@@ -25,9 +26,41 @@ function curtirPublicacao(evento){
         url: `/publicacoes/${publicacaoId}/curtir`,
         method: "POST"
     }).done(function(){ //200 OK
-        window.location = '/home';
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+        contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+
+        elementoClicado.addClass('descurtir-publicacao');
+        elementoClicado.addClass('text-danger');
+        elementoClicado.removeClass('curtir-publicacao');
+
+
     }).fail(function(erro){ //4.. 5.. ERRO
         alert("Curtida falhou!");
+    }).always(function(){
+        elementoClicado.prop('disabled',false);
+    })
+    
+}
+
+function descurtirPublicacao(evento){
+    evento.preventDefault();
+    const elementoClicado = $(evento.target);
+    elementoClicado.prop('disabled',true);
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/descurtir`,
+        method: "POST"
+    }).done(function(){ //200 OK
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+        contadorDeCurtidas.text(quantidadeDeCurtidas - 1);
+
+        elementoClicado.removeClass('descurtir-publicacao');
+        elementoClicado.removeClass('text-danger');
+        elementoClicado.addClass('curtir-publicacao');
+    }).fail(function(erro){ //4.. 5.. ERRO
+        alert("Descurtida falhou!");
     }).always(function(){
         elementoClicado.prop('disabled',false);
     })
