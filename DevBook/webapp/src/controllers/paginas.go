ef8040f962e977399e5c5e5120bf45a1d32e389e
Daniel Santos <dmsdaniel@gmail.com>
2023-03-07 20:01:49 -0300

    entrar pagina de login somente se estiver deslogado

--- DevBook/webapp/src/controllers/paginas.go
+++ DevBook/webapp/src/controllers/paginas.go
@@ -16,6 +16,11 @@ import (
 )
 
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
