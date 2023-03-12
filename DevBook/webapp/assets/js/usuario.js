b8d410be29b88ce74051eb1efcec83d29b957861
Daniel Santos <dmsdaniel@gmail.com>
2023-03-12 09:58:29 -0300

    atualizar senha usuario

--- DevBook/webapp/assets/js/usuario.js
+++ DevBook/webapp/assets/js/usuario.js
@@ -1,6 +1,7 @@
 $('#parar-de-seguir').on('click',pararDeSeguir)
 $('#seguir').on('click',seguir)
 $('#editar-usuario').on('submit', editar)
+$('#atualizar-senha').on('submit', atualizarSenha)
 function pararDeSeguir(){
     const usuarioId = $(this).data('usuario-id');
     $(this).prop('disabled',true);
@@ -51,4 +52,30 @@ function editar(evento){
     }).fail(function() {
         Swal.fire("Ops..", "Erro ao atualizar o usuário","error");
     })
+}
+
+function atualizarSenha(evento){
+    evento.preventDefault();
+
+    if ($('#nova-senha').val() != $('#confirmar-senha').val()) {
+        Swal.fire("Ops..", "Nova senha não confere", "warning");
+        return;
+    }
+
+    $.ajax({
+        url: "/atualizar-senha",
+        method: "POST",
+        data: {
+            atual: $('#senha-atual').val(),
+            nova: $('#nova-senha').val()
+        }
+    }).done(function(){
+        Swal.fire("Sucesso!", "Senha atualizada com sucesso", "success")
+        .then(function() {
+            window.location = "/perfil";
+        }).fail(function(){
+            Swal.fire("Ops..", "Erro ao atualizar a senha", "error");            
+        })
+    })
+
 }
